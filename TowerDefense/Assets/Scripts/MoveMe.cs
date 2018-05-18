d1508b90fa0cb3e5a3812ca62a83e605faef33d7
GTW-design <39174626+GTW-design@users.noreply.github.com>
2018-05-18 13:16:17 +1000

    First edit of Tower Defence, added towers and enemy alpha models

---
+++ TowerDefense/Assets/Scripts/MoveMe.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //The W key is pressed to move character forward
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(0, 0, 0.2f);
+        }
+        //The S key is pressed to move character backward
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(0, 0, -0.2f);
+        }
+        //The A key is pressed to move character to the left
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-0.2f, 0, 0);
+        }
+        //The D key is pressed to move character to the right
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(0.2f, 0, 0);
+        }
+	}
+}
